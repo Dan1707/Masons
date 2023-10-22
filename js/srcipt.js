f78580d1921b2804c1e1103652926d23e78ef50c
Dan1707 <danuhae@gmail.com>
2023-10-22 20:01:27 +0300

    example slider finished

--- js/srcipt.js
+++ js/srcipt.js
@@ -57,3 +57,48 @@
     });
   });
 }
+// EXAMPLE SLIDER
+{
+  window.addEventListener("DOMContentLoaded", () => {
+    const sliderWrapper = document.querySelector(
+        ".example__slider_item-wrapper"
+      ),
+      sliderItems = document.querySelectorAll(".example__slider_item"),
+      sliderPrevBtn = document.querySelectorAll(".example__slider_btn")[0],
+      sliderNextBtn = document.querySelectorAll(".example__slider_btn")[1],
+      sliderItemWidth = window.getComputedStyle(sliderItems[0]).width,
+      sliderItemGap = window.getComputedStyle(sliderWrapper).gap,
+      width =
+        +sliderItemWidth.slice(0, sliderItemWidth.length - 2) +
+        +sliderItemGap.slice(0, sliderItemGap.length - 2),
+      sliderFieldWidth = window.getComputedStyle(
+        document.querySelector(".example__slider_field")
+      ).width,
+      sliderQuantityCounter =
+        +sliderFieldWidth.slice(0, sliderFieldWidth.length - 2) / width,
+      sliderItemToShow = Math.round(sliderQuantityCounter),
+      sliderClicks = sliderItems.length - sliderItemToShow;
+
+    let offSet = 0;
+
+    sliderPrevBtn.addEventListener("click", () => {
+      if (offSet === 0) {
+        offSet = width * sliderClicks;
+      } else offSet -= width;
+
+      sliderWrapper.style.transform = `translateX(-${offSet}px)`;
+      console.log(offSet);
+    });
+
+    sliderNextBtn.addEventListener("click", () => {
+      if (offSet >= width * sliderClicks) {
+        offSet = 0;
+      } else {
+        offSet += width;
+      }
+
+      sliderWrapper.style.transform = `translateX(-${offSet}px)`;
+      console.log(offSet);
+    });
+  });
+}
